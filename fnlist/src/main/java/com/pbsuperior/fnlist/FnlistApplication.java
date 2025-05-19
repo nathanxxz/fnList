6aa27c00c0a35ce1d73f6b046ef769d37802ec49
Fabricio Nathan <fabricionathan749@gmail.com>
2025-05-19 13:22:39 -0300

    Classe game concluida

---
+++ fnlist/src/main/java/com/pbsuperior/fnlist/FnlistApplication.java
@@ -0,0 +1,13 @@
+package com.pbsuperior.fnlist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FnlistApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FnlistApplication.class, args);
+	}
+
+}
